ca7c0be860a1518ff583c9605d0da35b28057bfe
junshern <chanjunshern@gmail.com>
2016-09-18 09:47:40 +0100

    Changed MIDI output selection text for better UI

--- midi.py
+++ midi.py
@@ -11,7 +11,7 @@ class Midi(object):
         synthReady = False
         while (not synthReady):
             # Automatically look for a "Synth" output
-            print "Checking for MIDI outputs:"
+            print "Checking for \'Synth\' in available MIDI outputs:"
             for i in range(0,pygame.midi.get_count()):
                 # get_device_info(i) returns a list of info, [1] gives the device namestring
                 print "%i: %s" %(i, pygame.midi.get_device_info(i)[1])
@@ -20,7 +20,7 @@ class Midi(object):
                     synthReady = True
             # If no "Synth" output, prompt user for choice
             if (not synthReady):
-                selection = int(raw_input("Select output number >> "))
+                selection = int(raw_input("None found. Please select an output >> "))
                 if selection in range(0,pygame.midi.get_count()):
                     synthReady = True
                 else: 
